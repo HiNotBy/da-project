0a76a64c848856ced58ca900cecf800a4ff50d47
HiNotBy <gzarate@mmsd.org>
2026-03-17 19:25:43 +0000

    uh huh

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let x = 250;
 let y = 250;
+
 const canvas = document.getElementById("turtle");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
@@ -24,14 +25,28 @@ window.addEventListener('keydown', function(event) {
 
 window.addEventListener('keydown', function(event) {
     if(event.key === "ArrowRight") {
-        x += 50;
+        x += 50
     }
 });
 
-function runGame(){
-        ctx.clearRect(0, 0, 500, 500);
-        ctx.fillRect(x, y, 50, 50);
-        requestAnimationFrame(runGame);
-}
+yes();
 
-runGame();
+function gameLoop(){
+    ctx.clearRect(0, 0, 500, 500);
+    for(var i = 0; i < 500; i = i + 1){
+        for(var n = 0; n < 500; n = n + 1){
+            ctx.fillStyle = "red"
+            ctx.fillRect(n*100, i*100, 50,50);
+        }
+    }
+
+    for(var i = 0; i < 500; i = i + 1){
+        for(var n = 0; n < 500; n = n + 1){
+            ctx.fillStyle = "blue"
+            ctx.fillRect((n*100 + 50), (i*100 + 50), 50,50);
+        }
+    }
+    ctx.fillRect(x-25, y-25, 50, 50);
+
+    requestAnimationFrame(gameloop());
+}
