e796f04192615ba7a6592c75bee5916eea321f1c
HiNotBy <gzarate@mmsd.org>
2026-03-30 19:52:00 +0000

    foooooox
    Co-authored-by: F3rits <F3rits@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let maxx = 23;
 let maxy = 9;
 let tileSize = 50;
-let colors = ["#813c3c78", "#aa6666"];
+let colors = ["#813c3c78", "#aa6666", "#ff9999"];
 
 let playerA = {
-  x: 19,
+  x: 23,
   y: 9,
   width: 50,
   height: 50
@@ -33,35 +33,6 @@ let fire_shooty_map = [
   1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,
   0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1
 ];
-
-
-function drawMap(gameMap, fireRow, fireColumn) {
-  let cols = 24;
-  let rows = 10;
-  let size = 50;
-
-  let index = 0;
-
-  for (let y = 0; y < rows; y++) {
-    
-    for (let x = 0; x < cols; x++) {
-      if((x === fireColumn || y === fireRow)){
-        ctx.fillStyle = "#ff9500";
-      if(x === playerA.x && y === playerA.y){
-        alert("playerA is dead");
-      }
-      if(x === playerB.x && y === playerB.y){
-        alert("playerB is dead");
-      }
-      } else {
-        ctx.fillStyle = colors[gameMap[index]];
-      }
-      ctx.fillRect(x * size, y * size, size, size);
-      index++;
-    }
-  }
-}
-
 
 function playerCollision(newx, newy, other) {
   if(newx === other.x && newy === other.y){
@@ -126,21 +97,50 @@ window.addEventListener("keydown", function(event) {
   }
 });
 
-let fireIndex = 0
+function drawMap(gameMap) {
+  
+  let cols = 24;
+  let rows = 10;
+  let size = 50;
+  let index = 0;
+  
+  if (gameMap = fire_shooty_map){
+    let fireIndex = 0;
+    let fireRow = Math.floor(Math.random()*10);
+    let fireColumn = Math.floor(Math.random()*24);
+    const delay = 100; 
 
-const delay = 300;
+    for (let y = 0; y < rows; y++) {
+      for (let x = 0; x < cols; x++) {
+        if((x === fireColumn || y === fireRow)){
+          ctx.fillStyle = colors[2];
+        if(x === playerA.x && y === playerA.y){
+          alert("playerA is dead");
+        }
+        if(x === playerB.x && y === playerB.y){
+          alert("playerB is dead");
+        }
+        } else {
+          ctx.fillStyle = colors[gameMap[index]];
+        }
+        ctx.fillRect(x * size, y * size, size, size);
+        index++;
+      }
+     }
+
+     index++
+  }
+
+
+
+}
+
 function gameLoop() {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  if(fireIndex > delay){
-    if(fireIndex > delay + 10){
-      fireIndex = 0;
-    }
-    drawMap(fire_shooty_map, 6, 7);
-  } else {
-    drawMap(fire_shooty_map);
-  }
-    fireIndex ++;
+
+  drawMap(fire_shooty_map);
+
   ctx.fillStyle = "red";
   ctx.fillRect(playerA.x * 50, playerA.y * 50, playerA.width, playerA.height);
 
